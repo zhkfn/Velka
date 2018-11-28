b7d0e2c3805d87c5e4cad5c03cf244153315843b
Danielle <yayforpurple@gmail.com>
2018-11-28 13:02:43 -0600

    Delete more

--- BookOfJudgement/BookOfJudgement.py
+++ BookOfJudgement/BookOfJudgement.py
@@ -36,81 +36,6 @@ class Velka:
             await send_cmd_help(ctx)
             return
     
-    
-    # Edit score types
-    @velkaset.command(pass_context=True, name="scoreEditType")
-    async def _velkaset_scoreEditType(self, ctx, scoreType : str):
-        """- Edit the categories of scores"""
-        if scoreType:
-            for st in self.settings["SCORE_TYPE"]:
-                if st == scoreType:
-                    msg = "Currently Editing " + st + ".\n"
-                    msg += "Which property do you want to edit?"
-                    msg += "\n    1. Counter Noun: " + str(self.settings["SCORE_TYPE"][st]["noun"])
-                    msg += "\n    2. Emote ID: " + str(self.settings["SCORE_TYPE"][st]["emoteID"])
-                    msg += "\n    3. Weekly Decay Rate: " + str(self.settings["SCORE_TYPE"][st]["decayRate"])
-                    msg += "\n    4. Daily Limit: " + str(self.settings["SCORE_TYPE"][st]["dailyLimit"])
-                    msg += "\n    5. Award Role: " + str(self.settings["SCORE_TYPE"][st]["role"])
-                    msg += "\n    6. Role Cost: " + str(self.settings["SCORE_TYPE"][st]["roleCost"])
-                    msg += "\n    0. Exit"
-                    await self.bot.say(msg)
-                    msg = await self.bot.wait_for_message(author=ctx.message.author, timeout=60)
-                    if msg is None:
-                        await self.bot.say("Nothing selected. Quitting edit mode.")
-                        return
-                    if str.isdigit(msg) and int(msg) > 0 and int(msg) < 7:
-                        sel = int(msg)
-                        await self.bot.say("What value should it be set to?")
-                        val = await self.bot.wait_for_message(author=ctx.message.author, timeout=60)
-                        if val is None:
-                            await self.bot.say("No value given. Quitting edit mode.")
-                            return
-                        elif sel == 1:
-                            self.settings["SCORE_TYPE"][st]["noun"] = val
-                        elif sel == 2:
-                            self.settings["SCORE_TYPE"][st]["emoteID"] = val
-                        elif sel == 5:
-                            self.settings["SCORE_TYPE"][st]["role"] = val
-                        elif str.isdigit(val):
-                            if sel == 3:
-                                self.settings["SCORE_TYPE"][st]["decayRate"] = int(val)
-                            elif sel == 4:
-                                self.settings["SCORE_TYPE"][st]["dailyLimit"] = int(val)
-                            elif sel == 6:
-                                self.settings["SCORE_TYPE"][st]["roleCost"] = int(val)
-                        else:
-                            await self.bot.say("Invalid value.")
-                            _velkaset_scoreEditType(self, ctx, scoreType)
-                            return
-                        saveSettings(self)
-                        await self.bot.say("Value saved.")
-                        _velkaset_scoreEditType(self, ctx, scoreType)
-                        return
-                    if msg == "0":
-                        await self.bot.say("Quitting edit mode.")
-                        return
-                    await self.bot.say("Invalid selection. Quitting edit mode.")
-                    return
-            await self.bot.say("That score type does not exist.")
-        else:
-            msg ="Which score type would you like to edit?"
-            num = 0
-            for st in self.settings["SCORE_TYPES"]:
-                msg += "\n    " + st["command"]
-                num += 1
-            if num < 1:
-                await self.bot.say("You have not defined any scores yet. Please create one first.")
-                return
-            await self.bot.say(msg)
-            msg = await self.bot.wait_for_message(author=ctx.message.author, timeout=60)
-            if msg is None:
-                await self.bot.say("No score type selected. Quitting edit mode.")
-                return
-            if msg.content.lower().strip() == "exit":
-                await self.bot.say("Quitting edit mode")
-                return
-            _velkaset_scoreEditType(msg)
-    
     # Create a new score type
     @velkaset.command(pass_context=True, name="scoreAddType")
     async def _velkaset_scoreAddType(self, ctx, command : str):
@@ -127,9 +52,9 @@ class Velka:
                 else:
                     await self.bot.say('No score type was added.')
             else:
-                self.settings['SCORE_TYPE'][command] = {"noun":"points", "emoteID":"0", "decayRate":2, "dailyLimit":2, "role":"", "roleCost":0}
-                for m in self.scores:
-                    self.scores[m][command] = 0
+                #self.settings['SCORE_TYPE'][command] = {"noun":"points", "emoteID":"0", "decayRate":2, "dailyLimit":2, "role":"", "roleCost":0}
+                #for m in self.scores:
+                #    self.scores[m][command] = 0
                 saveSettings(self)
                 saveScores(self)
                 await self.bot.say(command + " created.")
