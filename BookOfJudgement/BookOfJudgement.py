3b58b6ee8476f42c3ae371486011423a2e17d46d
Danielle <yayforpurple@gmail.com>
2018-12-04 14:26:06 -0600

    Update BookOfJudgement.py

--- BookOfJudgement/BookOfJudgement.py
+++ BookOfJudgement/BookOfJudgement.py
@@ -483,7 +483,7 @@ class Velka:
     @velkaset.command(pass_context=True, name="backup")
     async def _velkaset_backup(self, ctx):
         """Back up scores and settings"""
-         await self.backup(ctx.message.channel)
+        await self.backup(ctx.message.channel)
     
     async def backup(self, channel):
         await self.bot.send_file(channel,"data/judgement/scores.json")
